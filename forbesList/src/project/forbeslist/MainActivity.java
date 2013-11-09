9cdbd8dfdabc818a4cacc385ffc2b5b2a2febbd1
SungJae <sungjael@andrew.cmu.edu>
2013-11-08 19:36:24 -0500

    main - register button listener added

--- forbesList/src/project/forbeslist/MainActivity.java
+++ forbesList/src/project/forbeslist/MainActivity.java
@@ -2,18 +2,37 @@ package project.forbeslist;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	Button joinButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        addListenerOnButton();
     }
 
 
+	public void addListenerOnButton() {
+		final Context context = this;
+		joinButton = (Button) findViewById(R.id.register);
+		joinButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+					// go to next activity with 3 values passed
+					Intent intent = new Intent(context, JoinActivity.class);
+					startActivity(intent);
+			}
+		});
+	}
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
