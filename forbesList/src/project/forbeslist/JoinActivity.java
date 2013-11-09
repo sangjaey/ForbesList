fabd21cc836c12b0ad16a907cb1d62c29d39e4e3
SungJae <sungjael@andrew.cmu.edu>
2013-11-09 15:19:18 -0500

    sss

--- forbesList/src/project/forbeslist/JoinActivity.java
+++ forbesList/src/project/forbeslist/JoinActivity.java
@@ -15,6 +15,7 @@ public class JoinActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_join);
+		addListenerOnButton();
 	}
 
 	public void addListenerOnButton() {
--- forbesList/src/project/forbeslist/MainActivity.java
+++ forbesList/src/project/forbeslist/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         addListenerOnButton();
+        addListenerOnButton2();
     }
 
 
