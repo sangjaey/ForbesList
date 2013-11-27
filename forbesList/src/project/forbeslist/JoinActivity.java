6ae6490210093ea0ca97ab5e4dd28e686712c752
Sangjae Yoo <sangjaey@cmu-783989.wv.cc.cmu.edu>
2013-11-27 17:53:51 -0500

    Join activity with simple registration

--- forbesList/src/project/forbeslist/JoinActivity.java
+++ forbesList/src/project/forbeslist/JoinActivity.java
@@ -3,16 +3,15 @@ package project.forbeslist;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.cloudbase.CBHelper;
-
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class JoinActivity extends Activity {
 	Button button1;
@@ -30,14 +29,33 @@ public class JoinActivity extends Activity {
 			@Override
 			public void onClick(View arg0) {
 					// go to next activity with 3 values passed
-				
+					EditText emailText = (EditText)findViewById(R.id.editText1);
+					EditText nameText = (EditText)findViewById(R.id.editText2);
+					EditText phoneText = (EditText)findViewById(R.id.editText3);
+					EditText passwordText = (EditText)findViewById(R.id.editText4);
+					
+					String email = emailText.getText().toString();
+					String name = nameText.getText().toString();
+					String phonenumber = phoneText.getText().toString();
+					String password = passwordText.getText().toString();
+					if(email.isEmpty()||
+							name.isEmpty()||
+							phonenumber.isEmpty()||
+							password.isEmpty()){
+						return;
+					}
+					
 				 	JSONObject in = null;
 
-			    	MainActivity.myHelper.setPassword(Util.md5("1234"));
+//			    	MainActivity.myHelper.setPassword(Util.md5("1234"));
 			    	try {
-						 in= new JSONObject().put("name2", "sangwon2");
+			    		 in = new JSONObject();
+			    		 in.put("email", email);
+			    		 in.put("name", name);
+			    		 in.put("phone", phonenumber);
+			    		 in.put("password", password);
+//						 in= new JSONObject().put("name2", "sangwon2");
 					} catch (JSONException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 			    	MainActivity.myHelper.insertDocument(in, "test");
