ef57a4900806e955889151cf0157e6a6783ef4d5
SungJae <sungjael@andrew.cmu.edu>
2013-11-27 13:24:53 -0500

    search done

--- forbesList/src/project/forbeslist/MainActivity.java
+++ forbesList/src/project/forbeslist/MainActivity.java
@@ -3,9 +3,11 @@
 package project.forbeslist;
 
 import com.cloudbase.*;
-import com.google.gson.JsonObject;
+import com.cloudbase.datacommands.CBSearchCondition;
+import com.cloudbase.datacommands.CBSearchConditionOperator;
 
-
+import org.json.JSONException;
+import org.json.JSONObject;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
@@ -14,10 +16,10 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import java.io.FileInputStream;
-import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
 import android.content.SharedPreferences;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -25,9 +27,6 @@ import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import java.io.UnsupportedEncodingException;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import android.widget.Toast;
 
@@ -42,19 +41,28 @@ public class MainActivity extends Activity implements CBHelperResponder {
         setContentView(R.layout.activity_main);
         addListenerOnButton();
         addListenerOnButton2();
-        JSONObject in = null;
+        JSONObject in = new JSONObject();;
     	myHelper = new CBHelper(
     	        "forbeslist",
     	        "cac21deef7dd81d8af7506cd257173d2", 
     	        this);
     	myHelper.setPassword(md5("1234"));
     	try {
-			 in= new JSONObject().put("name", "sangwon");
+    		in.put("name", "sangwon"); 
+    		in.put("pw","123123");
+			 //in= new JSONObject().put("name", "sangwon");
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
     	myHelper.insertDocument(in, "test",this);
+
+    	
+    	// search
+    	CBSearchCondition cond = new CBSearchCondition("nameValuePairs.name", CBSearchConditionOperator.CBOperatorEqual, "sangwon");
+        cond.setLimit(3);
+
+        myHelper.searchDocument("test",cond,this);
     	
     
     }
@@ -110,8 +118,15 @@ public class MainActivity extends Activity implements CBHelperResponder {
     }
 	@Override
 	public void handleResponse(CBQueuedRequest arg0, CBHelperResponse arg1) {
-		// TODO Auto-generated method stub
-		
+
+		if(arg1.getData() instanceof List){
+			System.out.println("@@@@@@@@@@@@@@@@@@@"+ ((List)arg1.getData()).size());
+			System.out.println("*******************"+((List)arg1.getData()).get(0).toString());
+		}
+			else
+			System.out.println("###################"+arg1.getData());
+			
+			
 	}
     
 }
