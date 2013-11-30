b26b7753174a2084fa03c83343fcfcdc9047bb05
SungJae <sungjael@andrew.cmu.edu>
2013-11-30 01:46:52 -0500

    login checking pw success

--- forbesList/src/project/forbeslist/MainActivity.java
+++ forbesList/src/project/forbeslist/MainActivity.java
@@ -36,13 +36,17 @@ import android.widget.Toast;
 public class MainActivity extends Activity implements CBHelperResponder {
 	Button joinButton, button1;
 	public static CBHelper myHelper;
-
+	String pw;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		addListenerOnButton();
 		addListenerOnButton2();
+		myHelper = new CBHelper("forbeslist",
+				"cac21deef7dd81d8af7506cd257173d2", this);
+		myHelper.setPassword(md5("1234"));
 
 		
 		/*
@@ -82,34 +86,19 @@ public class MainActivity extends Activity implements CBHelperResponder {
 	// try to log in
 	public void addListenerOnButton2() {
 		final Context context = this;
-		myHelper = new CBHelper("forbeslist",
-				"cac21deef7dd81d8af7506cd257173d2", this);
-		myHelper.setPassword(md5("1234"));
-
-		myHelper = new CBHelper("forbeslist",
-				"cac21deef7dd81d8af7506cd257173d2", this);
-		myHelper.setPassword(md5("1234"));
-		User in = new User("a","b","c");
-		
-		myHelper.insertDocument(in, "test", this);
-		
-		
-		
 		
 		button1 = (Button) findViewById(R.id.button1);
 		button1.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View arg0) {
 				// go to next activity with 3 values passed
-				// Intent intent = new Intent(context, TabMainActivity.class);
-				// startActivity(intent);
+				// 
 				EditText emailText = (EditText) findViewById(R.id.email);
 				EditText passwordText = (EditText) findViewById(R.id.pw);
 
 				String email = emailText.getText().toString();
-				String password = passwordText.getText().toString();
+				pw = passwordText.getText().toString();
 
-				System.out.println(email);
 				CBSearchCondition cond = new CBSearchCondition(
 						"email",
 						CBSearchConditionOperator.CBOperatorEqual, email);
@@ -131,12 +120,16 @@ public class MainActivity extends Activity implements CBHelperResponder {
 	public void handleResponse(CBQueuedRequest arg0, CBHelperResponse arg1) {
 
 		if (arg1.getData() instanceof List) {
-			System.out.println("@@@@@@@@@@@@@@@@"
-					+ ((List) arg1.getData()).size());
 			if (((List) arg1.getData()).size() != 0) {
 				String s = (((List) arg1.getData()).get(0)).toString();
-				System.out.println(((com.google.gson.internal.StringMap)((List)arg1.getData()).get(0)).get("email"));
-				
+				String pass = (String)((com.google.gson.internal.StringMap)((List)arg1.getData()).get(0)).get("password");
+				if(pw.equals(pass)){
+					Intent intent = new Intent(this, TabMainActivity.class);
+					startActivity(intent);
+				}
+				else{
+					Toast.makeText(this, "wrong password", Toast.LENGTH_LONG);
+				}
 						
 	/*			JSONObject result;
 				try {
