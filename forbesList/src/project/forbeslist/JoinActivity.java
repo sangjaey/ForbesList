ff2535ba86d3d3542b9197e0393c5b0cff23af65
Sangjae Yoo <sangjaey@cmu-783989.wv.cc.cmu.edu>
2013-11-30 14:03:18 -0500

    join handles duplicate email

--- forbesList/src/project/forbeslist/JoinActivity.java
+++ forbesList/src/project/forbeslist/JoinActivity.java
@@ -1,7 +1,6 @@
 package project.forbeslist;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+import java.util.List;
 
 import android.app.Activity;
 import android.content.Context;
@@ -12,12 +11,21 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
-public class JoinActivity extends Activity {
+import com.cloudbase.CBHelperResponder;
+import com.cloudbase.CBHelperResponse;
+import com.cloudbase.CBQueuedRequest;
+import com.cloudbase.datacommands.CBSearchCondition;
+import com.cloudbase.datacommands.CBSearchConditionOperator;
+
+public class JoinActivity extends Activity implements CBHelperResponder{
 	Button button1;
+	private boolean validEmail;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		validEmail = false;
 		setContentView(R.layout.activity_join);
 		addListenerOnButton();
 	}
@@ -28,48 +36,71 @@ public class JoinActivity extends Activity {
 		button1.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View arg0) {
-					// go to next activity with 3 values passed
-					EditText emailText = (EditText)findViewById(R.id.editText1);
-					EditText nameText = (EditText)findViewById(R.id.editText2);
-					EditText phoneText = (EditText)findViewById(R.id.editText3);
-					EditText passwordText = (EditText)findViewById(R.id.editText4);
-					
-					String email = emailText.getText().toString();
-					String name = nameText.getText().toString();
-					String phonenumber = phoneText.getText().toString();
-					String password = passwordText.getText().toString();
-					if(email.isEmpty()||
-							name.isEmpty()||
-							phonenumber.isEmpty()||
-							password.isEmpty()){
-						return;
-					}
-					
-				 	JSONObject in = null;
+				// go to next activity with 3 values passed
+				EditText emailText = (EditText)findViewById(R.id.editText1);
+				EditText nameText = (EditText)findViewById(R.id.editText2);
+				EditText phoneText = (EditText)findViewById(R.id.editText3);
+				EditText passwordText = (EditText)findViewById(R.id.editText4);
 
-//			    	MainActivity.myHelper.setPassword(Util.md5("1234"));
-			    	try {
-			    		 in = new JSONObject();
-			    		 in.put("email", email);
-			    		 in.put("name", name);
-			    		 in.put("phone", phonenumber);
-			    		 in.put("password", password);
-//						 in= new JSONObject().put("name2", "sangwon2");
-					} catch (JSONException e) {
-						e.printStackTrace();
-					}
-			    	MainActivity.myHelper.insertDocument(in, "test");
-					Intent intent = new Intent(context, TabMainActivity.class);
-					startActivity(intent);
+				String email = emailText.getText().toString();
+				String name = nameText.getText().toString();
+				String phonenumber = phoneText.getText().toString();
+				String password = passwordText.getText().toString();
+				if(email.isEmpty()||
+						name.isEmpty()||
+						phonenumber.isEmpty()||
+						password.isEmpty()){
+					return;
+				}
+				CBSearchCondition cond = new CBSearchCondition(
+						"email",
+						CBSearchConditionOperator.CBOperatorEqual, email);
+				MainActivity.myHelper.searchDocument("test", cond, JoinActivity.this);
+
+				if(!validEmail){
+					TextView error_text = (TextView)findViewById(R.id.register_error);
+					error_text.setText("Email Already In use");
+					return;
+				}
+				User newUser = new User(email, password, name);
+
+				MainActivity.myHelper.insertDocument(newUser, "test");
+				Intent intent = new Intent(context, TabMainActivity.class);
+				startActivity(intent);
 			}
 		});
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.join, menu);
 		return true;
+	}
+
+	@Override
+	public void handleResponse(CBQueuedRequest arg0, CBHelperResponse arg1) {
+		
+		// TODO Auto-generated method stub
+		if(arg1==null){
+			validEmail = true;
+		}
+		
+		if (arg1.getData() instanceof List) {
+			List results = (List) arg1.getData();
+			if (results.size() == 0) {
+				validEmail = true;
+			}
+			for(int i=0;i<results.size();i++){
+				String email = (String)((com.google.gson.internal.StringMap)((List)arg1.getData()).get(i)).get("email");
+				System.out.println("Printing emails     "+i+":" + email);
+			}
+			
+		} else{
+			System.out.println("###################" + arg1.getData());			
+		}
+
+
 	}
 
 }
